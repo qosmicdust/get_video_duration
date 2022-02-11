396ed0e44a4f2fd19dc227a986828fda31c0ca03
qosmicdust <cosmicdust@hutom.co.kr>
2022-02-11 09:40:56 +0900

    new: get_video_duration.py

---
+++ get_video_duration.py
@@ -0,0 +1,19 @@
+from subprocess import CalledProcessError, check_output
+
+
+def get_duration(src):
+
+    try:
+        a = check_output(f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal "{src}"', shell=True)
+        return a.decode().strip()
+
+    except CalledProcessError as e:
+        print(e.output)
+        a = None
+        return a
+
+
+if __name__=='__main__':
+
+    src = 'your/video/path.mp4'
+    print(get_duration(src))
