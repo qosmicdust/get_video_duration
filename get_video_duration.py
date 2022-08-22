720be2468b70c10eace465046975ce6ad3588ee7
qosmicdust <cosmicdust@hutom.co.kr>
2022-08-22 10:16:40 +0900

    refactoring

--- get_video_duration.py
+++ get_video_duration.py
@@ -4,7 +4,10 @@ from subprocess import CalledProcessError, check_output
 def get_duration(src):
 
     try:
-        a = check_output(f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 -sexagesimal "{src}"', shell=True)
+        a = check_output(
+            f'ffprobe -v error -show_entries format=duration '
+            f'-of default=noprint_wrappers=1:nokey=1 -sexagesimal "{src}"',
+            shell=True)
         return a.decode().strip()
 
     except CalledProcessError as e:
@@ -13,7 +16,7 @@ def get_duration(src):
         return a
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
 
     src = 'your/video/path.mp4'
     print(get_duration(src))
